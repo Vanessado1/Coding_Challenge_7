8599a9819d89b70ca62f6180d26a3ce6494b8221
Vanessa Do <xex.vanessa@gmail.com>
2025-02-11 18:14:21 -0500

    Task 1- Customer Invoice Calculation

---
+++ cc_7.js
@@ -0,0 +1,7 @@
+// Task 1: Function Declaration 
+//declared calculateInvoice function 
+ function calculateInvoice(subtotal, taxRate, discount) {
+    return total  = (subtotal + (subtotal * taxRate)) - discount
+ }
+console.log(`Total Invoice: $${calculateInvoice(100, 0.08, 5)}`); // Expected output: "Total Invoice: $103.00"
+console.log(`Total Invoice: $${calculateInvoice(500, 0.1, 20)}`); // Expected output: "Total Invoice: $530.00"
