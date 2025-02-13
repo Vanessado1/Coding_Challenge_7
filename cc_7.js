b1347b0911af1ea6f92132929b246252aadbdfbb
Vanessa Do <xex.vanessa@gmail.com>
2025-02-13 17:57:52 -0500

    Edited tasks 1 and 2

--- cc_7.js
+++ cc_7.js
@@ -1,7 +1,9 @@
 // Task 1: Function Declaration 
 //declared calculateInvoice function 
  function calculateInvoice(subtotal, taxRate, discount) {
-    return total  = (subtotal + (subtotal * taxRate)) - discount
+    let total = 0
+   total = (subtotal + (subtotal * taxRate)) - discount
+    return total.toFixed(2)
  }
 console.log(`Total Invoice: $${calculateInvoice(100, 0.08, 5)}`); // Expected output: "Total Invoice: $103.00"
 console.log(`Total Invoice: $${calculateInvoice(500, 0.1, 20)}`); // Expected output: "Total Invoice: $530.00"
@@ -9,7 +11,9 @@ console.log(`Total Invoice: $${calculateInvoice(500, 0.1, 20)}`); // Expected ou
 // Task 2: Function Expression 
 //declared calculateHourlyWage 
 function calculateHourlyWage(salary, hoursPerWeek) {
-   return Math.round (hourlyWage = salary / (hoursPerWeek * 52))
+   let totalHours = 0
+   totalHours= (hourlyWage = salary / (hoursPerWeek * 52))
+   return totalHours.toFixed(2)
 }
 console.log(`Hourly Wage: $${calculateHourlyWage(52000, 40)}`); // Expected output: "Hourly Wage: $25.00"
 console.log(`Hourly Wage: $${calculateHourlyWage(75000, 35)}`); // Expected output: "Hourly Wage: $41.21"
